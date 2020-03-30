622a39705d13c69324acfd884deb070505665e19
Pavan Patharde <pavan.patharde@netapp.com>
2020-03-30 19:57:39 +0530

    Added new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello,Pavan"); 
+        System.out.println("Hello,Pavan");
+	System.out.println("2+2=4") 
     } 
 } 
